702c907d45f7faa395528ef2e7c658a8cc7b9ed8
RKCodesTech <riyakosta2609@gmail.com>
2026-06-16 23:33:58 +0530

    Depedency Injection by setter method using java config file

--- Spring7/src/main/java/in/sp/config/SpirngConfilgfile.java
+++ Spring7/src/main/java/in/sp/config/SpirngConfilgfile.java
@@ -1,5 +1,24 @@
 package in.sp.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import in.sp.beans.*;
+@Configuration
 public class SpirngConfilgfile {
+	@Bean
+	public Address Adrobj() {
+		Address adr=new Address();
+		adr.setCityname("Jhansi");
+		adr.setHouseno("345");
+		return adr;
+	}
+	@Bean
+	public Student Stdobj() {
+		Student std= new Student();
+		std.setName("Riya Kosta");
+		std.setAge("19");
+		std.setAddress(Adrobj());  //injected object of address class here
+		return std;
+	}
 
 }
