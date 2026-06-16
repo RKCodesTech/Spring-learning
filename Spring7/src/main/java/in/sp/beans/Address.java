49160675569ca236c32fd9b78b88ff98d19bb312
RKCodesTech <riyakosta2609@gmail.com>
2026-06-16 23:18:13 +0530

    Depedency Injection by setter method using java config file

--- Spring7/src/main/java/in/sp/beans/Address.java
+++ Spring7/src/main/java/in/sp/beans/Address.java
@@ -1,5 +1,13 @@
 package in.sp.beans;
 
 public class Address {
+private String cityname;
+private String houseno;
+public void setCityname(String cityname) {
+	this.cityname = cityname;
+}
+public void setHouseno(String houseno) {
+	this.houseno = houseno;
+}
 
 }
--- Spring7/src/main/java/in/sp/beans/Student.java
+++ Spring7/src/main/java/in/sp/beans/Student.java
@@ -1,5 +1,22 @@
 package in.sp.beans;
 
 public class Student {
-
+private String name;
+private int age;
+private Address address;
+public void setName(String name) {
+	this.name = name;
+}
+public void setAge(String age) {
+	this.age = age;
+}
+public void setAddress(Address address) {
+	this.address = address;
+}
+public void display() {
+	System.out.println("Name: "+name);
+	System.out.println("Age: "+age);
+	System.out.println("Address: "+address);
+	
+}
 }
