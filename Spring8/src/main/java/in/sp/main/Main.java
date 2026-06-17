e33bd4d2648f8c70ac0337809d60955e75167a51
RKCodesTech <riyakosta2609@gmail.com>
2026-06-17 23:45:56 +0530

    Depedency Injection by constructor method using Java config file

--- Spring8/src/main/java/in/sp/main/Main.java
+++ Spring8/src/main/java/in/sp/main/Main.java
@@ -3,9 +3,14 @@ package in.sp.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import in.sp.beans.Student;
+import in.sp.config.*;	
 
 public class Main {
-ApplicationContext context =new AnnotationConfigApplicationContext("configFile.java");
-Student std =(Student) context.getBean("stdId");
+	public static void main(String [] args) {
+		
+		ApplicationContext context =
+		        new AnnotationConfigApplicationContext(configFile.class);
+Student std =(Student) context.getBean("stdId1");
 std.display();
+}
 }
