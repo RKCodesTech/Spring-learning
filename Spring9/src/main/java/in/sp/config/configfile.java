e5e3ae22747313fe32a56b39bc9d010668ba34da
RKCodesTech <riyakosta2609@gmail.com>
2026-06-20 22:57:49 +0530

    Autowiring - means spring will inject dependency automatically

--- Spring9/src/main/java/in/sp/config/configfile.java
+++ Spring9/src/main/java/in/sp/config/configfile.java
@@ -1,5 +1,23 @@
 package in.sp.config;
-@Con
-public class configfile {
+import in.sp.beans.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class configfile {
+@Bean
+public Student obj1() {
+	Student std = new Student ();
+	std .setName("Riya");
+	std.setAge(19);
+	std.setAddress(obj2());
+	return std;
+}
+@Bean
+public Address obj2() {
+	Address adr= new Address();
+	adr.setCityname("Indore");
+	adr.setPincode(123456789);
+	return adr;
+}
 }
