60f02cd8f57d53d0ec8ddb703f489b849eb95c6f
RKCodesTech <riyakosta2609@gmail.com>
2026-06-22 23:17:40 +0530

    Spring10.java

--- Spring10/src/main/java/in/sp/beans/Subject.java
+++ Spring10/src/main/java/in/sp/beans/Subject.java
@@ -1,16 +1,16 @@
 package in.sp.beans;
 
+import java.util.List;
+
 public class Subject {
 
-private List<String> subjects;
+    private List<String> subjects;
 
-public List<String> getSubjects() {
-	return subjects;
-}
+    public List<String> getSubjects() {
+        return subjects;
+    }
 
-public void setSubjects(List<String> subjects) {
-	this.subjects = subjects;
-}
-
-
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
 }
