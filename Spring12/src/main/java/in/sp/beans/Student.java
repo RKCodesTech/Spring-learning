72f0ac78f5fbd6884f696e82979e4c7817b628df
RKCodesTech <riyakosta2609@gmail.com>
2026-06-24 23:54:03 +0530

    XML Based AutoWiring

--- Spring12/src/main/java/in/sp/beans/Student.java
+++ Spring12/src/main/java/in/sp/beans/Student.java
@@ -1,5 +1,32 @@
 package in.sp.beans;
 
 public class Student {
-
+private String name;
+private String email;
+private int rollno;
+private Address address;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Address getAddress() {
+	return address;
+}
+public void setAddress(Address address) {
+	this.address = address;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public int getRollno() {
+	return rollno;
+}
+public void setRollno(int rollno) {
+	this.rollno = rollno;
+}
 }
