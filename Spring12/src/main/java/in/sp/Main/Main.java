1129a2edda5616c2353209f97b76a3f43b3d2a34
RKCodesTech <riyakosta2609@gmail.com>
2026-06-25 21:46:37 +0530

    .

--- Spring12/src/main/java/in/sp/Main/Main.java
+++ Spring12/src/main/java/in/sp/Main/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 public static void main (String []args) {
-	ApplicationContext context =new ClassPathXmlApplicationContext("Student.class");
+	ApplicationContext context =new ClassPathXmlApplicationContext("ApplicationContext.xml");
 	Student std=(Student) context.getBean("StdId");
 	std.display();
 }
