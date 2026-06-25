3596c4a3b6c3fdd5261f8aedf652648fb50f7e48
RKCodesTech <riyakosta2609@gmail.com>
2026-06-25 21:39:47 +0530

    Autowiring by xml file

--- Spring12/src/main/java/in/sp/Main/Main.java
+++ Spring12/src/main/java/in/sp/Main/Main.java
@@ -1,5 +1,12 @@
 package in.sp.Main;
+import in.sp.beans.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-
+public static void main (String []args) {
+	ApplicationContext context =new ClassPathXmlApplicationContext("Student.class");
+	Student std=(Student) context.getBean("StdId");
+	std.display();
+}
 }
