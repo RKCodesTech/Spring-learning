e6942a04bcd7118b87e7e71cb56b62e58417a7e9
RKCodesTech <riyakosta2609@gmail.com>
2026-06-27 00:02:13 +0530

    .

--- Spring14/src/main/java/in/sp/beans/Student.java
+++ Spring14/src/main/java/in/sp/beans/Student.java
@@ -5,29 +5,11 @@ private String name;
 private String email;
 private int rollno;
 private Address address;
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public Address getAddress() {
-	return address;
-}
-public void setAddress(Address address) {
-	this.address = address;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public int getRollno() {
-	return rollno;
-}
-public void setRollno(int rollno) {
-	this.rollno = rollno;
+public Student(String name, String email, int rollno, Address address) {
+	this.name=name;
+	this.email=email;
+	this.rollno=rollno;
+	this.address=address;
 }
 public void display() {
 	System.out.print("Name: "+name);
