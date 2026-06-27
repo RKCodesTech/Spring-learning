78f8baff88e0ef9d9ee45ad3f207b4b83e9db9fe
RKCodesTech <riyakosta2609@gmail.com>
2026-06-27 23:24:02 +0530

    /

--- SpringCRUD/src/main/resources/SpringConfigFile.java
+++ SpringCRUD/src/main/resources/SpringConfigFile.java
@@ -1,4 +1,12 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+@Configuration
 public class SpringConfigFile {
-
+public DriverManagerDataSource myDataSource() {
+	DriverManagerDataSource datasource=new DriverManagerDataSource();
+	datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	return datasource; 
 }
+}
+s 
