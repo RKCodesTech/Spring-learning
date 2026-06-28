b8b014cc28db8f7456ad7fd1d635a37e6605a604
RKCodesTech <riyakosta2609@gmail.com>
2026-06-28 23:18:37 +0530

    h

--- SpringCRUD/src/main/java/in/sp/main/App.java
+++ SpringCRUD/src/main/java/in/sp/main/App.java
@@ -7,11 +7,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class App {
 	int std_rollno=2345;
 	String std_name="Riya";
-	String std_marks=345;
+	String std_marks=34547;
 public static void main (String[] args) {
 	ApplicationContext context=new AnnotationConfigApplicationContext(SpringConfigFile.class);
 	JdbcTemplate jdbcTemplate=context.getBean(JdbcTemplate.class);
 	String insert_sqlQuery="INSERT INTO student VALUES(?,?,?)";
-	jdbcTemplate.update(null);
+	jdbcTemplate.update(insert_sqlQuery);
 }
 }
