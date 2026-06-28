8b0f51732def6cba3d08e37bd1e8cdb84136dd0e
RKCodesTech <riyakosta2609@gmail.com>
2026-06-28 23:49:43 +0530

    t

--- SpringCRUD/src/main/java/in/sp/resources/SpringConfigFile.java
+++ SpringCRUD/src/main/java/in/sp/resources/SpringConfigFile.java
@@ -2,7 +2,7 @@ package in.sp.resources;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
+import org.springframework.context.annotation.Bean;
 @Configuration
 public class SpringConfigFile {
 	
