29de5a081ab762a7e02b57665bf6a27f689078b6
RKCodesTech <riyakosta2609@gmail.com>
2026-06-29 22:28:16 +0530

    .

--- NamedParameterjdbcTemplateDemo/src/main/java/in/sp/main/Main.java
+++ NamedParameterjdbcTemplateDemo/src/main/java/in/sp/main/Main.java
@@ -1,9 +1,9 @@
 package in.sp.main;
-
+import in.sp.config.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-import in.sp.resources.SpringConfigFile;
+
 public class Main {
 
 public static void main (String[] args) {
