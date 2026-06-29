9b78eb64e5c649258f332216e85b3bba5f20dc31
RKCodesTech <riyakosta2609@gmail.com>
2026-06-29 22:27:03 +0530

    /

--- NamedParameterjdbcTemplateDemo/src/main/java/in/sp/config/SpringConfigFile.java
+++ NamedParameterjdbcTemplateDemo/src/main/java/in/sp/config/SpringConfigFile.java
@@ -1,5 +1,28 @@
-package in.sp.config;
-
+import in.sp.config.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.context.annotation.Bean;
+@Configuration
 public class SpringConfigFile {
-
+	
+	
+	@Bean
+	// drivermanageer is that establish the connection between jdbc and application
+public DriverManagerDataSource myDataSource() {
+	DriverManagerDataSource datasource=new DriverManagerDataSource();
+	datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	datasource.setUrl("jdbc:mysql://localhost:3306/spring_jdbc_db");
+	datasource.setUsername("root");
+	datasource.setPassword("1234");
+	return datasource; 
 }
+
+//  
+	@Bean
+public JdbcTemplate myjdbcTemplate() {
+	JdbcTemplate jdbcTemplate=new JdbcTemplate();
+	jdbcTemplate.setDataSource(myDataSource()); //myDataSource() this will return driverMangerDataSource
+	return jdbcTemplate;
+}
+ }
--- NamedParameterjdbcTemplateDemo/src/main/java/in/sp/main/Main.java
+++ NamedParameterjdbcTemplateDemo/src/main/java/in/sp/main/Main.java
@@ -1,5 +1,41 @@
 package in.sp.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import in.sp.resources.SpringConfigFile;
 public class Main {
 
+public static void main (String[] args) {
+	int std_rollno=2345;
+	String std_name="Riya";
+	int std_marks=34547;
+	
+	ApplicationContext context=new AnnotationConfigApplicationContext(SpringConfigFile.class);
+	JdbcTemplate jdbcTemplate=context.getBean(JdbcTemplate.class);
+	String insert_sqlQuery="INSERT INTO student VALUES(?,?,?)";
+	int count =jdbcTemplate.update(insert_sqlQuery, std_rollno,std_name,std_marks);
+	if(count>0) {
+		System.out.println("insertion success");
+	}else {
+		System.out.println("insertion failed");
+	}
 }
+}
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
