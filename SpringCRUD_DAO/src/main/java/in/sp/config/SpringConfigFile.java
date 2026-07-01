6f3570ff37ee133755e96c7c97447bd7447b902e
RKCodesTech <riyakosta2609@gmail.com>
2026-07-01 23:50:44 +0530

    d

--- SpringCRUD_DAO/src/main/java/in/sp/config/SpringConfigFile.java
+++ SpringCRUD_DAO/src/main/java/in/sp/config/SpringConfigFile.java
@@ -1,5 +1,15 @@
 package in.sp.config;
 
-public class SpringConfigFile {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+@Configuration
+public class SpringConfigFile {
+@Bean
+public DriverManagerDataSource Datasource() {
+	DriverManagerDataSource drivermanager=new DriverManagerDataSource();
+	drivermanager.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	return drivermanager;
+}
 }
