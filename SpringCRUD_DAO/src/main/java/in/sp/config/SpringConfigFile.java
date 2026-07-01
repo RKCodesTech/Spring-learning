7cd630828d40303482da3259df271daffd66de28
RKCodesTech <riyakosta2609@gmail.com>
2026-07-01 23:59:28 +0530

    f

--- SpringCRUD_DAO/src/main/java/in/sp/config/SpringConfigFile.java
+++ SpringCRUD_DAO/src/main/java/in/sp/config/SpringConfigFile.java
@@ -1,7 +1,10 @@
 package in.sp.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
@@ -10,6 +13,14 @@ public class SpringConfigFile {
 public DriverManagerDataSource Datasource() {
 	DriverManagerDataSource drivermanager=new DriverManagerDataSource();
 	drivermanager.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	drivermanager.setUrl("jdbc:mysql://localhost:3306/spring_jdbc_db");
+	drivermanager.setUsername("root");
+	drivermanager.setPassword("1234");
 	return drivermanager;
+}
+@Bean
+public JdbcTemplate myjdbctemplate() {
+	JdbcTemplate jdbcTemplate=new JdbcTemplate();
+	jdbcTemplate.setDataSource(DataSource());
 }
 }
