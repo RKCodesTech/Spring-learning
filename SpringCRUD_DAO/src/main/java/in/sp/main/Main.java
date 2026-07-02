56b9bd79ec4e9d7af95dec7542f153aa0f35e80a
RKCodesTech <riyakosta2609@gmail.com>
2026-07-03 01:03:37 +0530

    s

--- SpringCRUD_DAO/src/main/java/in/sp/main/Main.java
+++ SpringCRUD_DAO/src/main/java/in/sp/main/Main.java
@@ -1,5 +1,20 @@
 package in.sp.main;
+import in.sp.dao.*;
+import in.sp.beans.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import in.sp.config.SpringConfigFile;
 
 public class Main {
+	public static void main(String[]args) {
+ApplicationContext context=new AnnotationConfigApplicationContext(SpringConfigFile.class);
+StudenDAO dao=context.getBean(StudetDAO.class);
+Student std=new Student();
+std.setStd_rollno(2345);
+std.setStd_name("Ritesh Kosta");
+std.setStd_marks(43543);
 
+
+}
 }
