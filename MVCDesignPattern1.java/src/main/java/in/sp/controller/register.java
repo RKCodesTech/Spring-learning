6621a1a8fedaa36378ffe1d9b663a776a79b836f
RKCodesTech <riyakosta2609@gmail.com>
2026-07-12 00:59:06 +0530

    mvc

--- MVCDesignPattern1.java/src/main/java/in/sp/controller/register.java
+++ MVCDesignPattern1.java/src/main/java/in/sp/controller/register.java
@@ -1,8 +1,18 @@
 package in.sp.controller;
 
+import java.net.http.HttpRequest;
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/Register")
 public class register extends HttpServlet {
-
+@Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws jakarta.servlet.ServletException ,java.io.IOException {
+	String myname=req.getParameter("name1");
+	String myemail=req.getParameter("email1");
+	int mypass=req.getParameter("password");
 }
+}
+;
