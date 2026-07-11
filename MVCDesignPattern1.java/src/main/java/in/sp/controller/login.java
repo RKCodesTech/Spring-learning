c210572ac52e4350d4fa932ef0ecd1197c0626b5
RKCodesTech <riyakosta2609@gmail.com>
2026-07-12 01:06:49 +0530

    writing servlet code

--- MVCDesignPattern1.java/src/main/java/in/sp/controller/login.java
+++ MVCDesignPattern1.java/src/main/java/in/sp/controller/login.java
@@ -1,5 +1,18 @@
 package in.sp.controller;
 
-public class login {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;          //mvc_db databse name
+
+@WebServlet("/LoginForm")
+public class login extends HttpServlet {
+@Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	super.doPost(req, resp);
+}
 }
