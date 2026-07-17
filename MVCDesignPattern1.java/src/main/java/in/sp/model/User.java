4a7c52d53dba30ae19961ba74825ccaede74be41
RKCodesTech <riyakosta2609@gmail.com>
2026-07-17 23:44:52 +0530

    update user.java

--- MVCDesignPattern1.java/src/main/java/in/sp/model/User.java
+++ MVCDesignPattern1.java/src/main/java/in/sp/model/User.java
@@ -1,22 +1,32 @@
 package in.sp.model;
 
-/**
- * 
- */
 public class User {
-private String name;
-private String email;
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-	
+
+    private String name;
+    private String email;
+
+    // Default Constructor
+    public User() {
+
+    }
+
+    // Getter for Name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for Name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for Email
+    public String getEmail() {
+        return email;
+    }
+
+    // Setter for Email
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
