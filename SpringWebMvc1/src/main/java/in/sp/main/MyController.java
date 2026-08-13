33d64a5b2bd20d3970947caa1af85f8009bfe64a
RKCodesTech <riyakosta2609@gmail.com>
2026-08-13 23:29:43 +0530

    create mycontorller.java for returning the model and view objects and index.html

---
+++ SpringWebMvc1/src/main/java/in/sp/main/MyController.java
@@ -0,0 +1,11 @@
+package in.sp.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+public ModelAndView openHelloPage() {
+	return null; 
+}
+}
