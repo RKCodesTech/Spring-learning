da8eec7337db1fe4e9b2a6bb4928af89012a078a
RKCodesTech <riyakosta2609@gmail.com>
2026-08-29 23:06:51 +0530

    implementation of StudentController.. where first add @controller to make normal class to controller and add request mapping to handle a request at a time

--- Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
+++ Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
@@ -1,5 +1,15 @@
 package com.riya.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
 public class StudentController {
 
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
 }
