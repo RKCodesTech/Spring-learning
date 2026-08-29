d737bd1b292ddc8a639cda85477882587a86a39d
RKCodesTech <riyakosta2609@gmail.com>
2026-08-30 00:23:34 +0530

    debug and fix errors project run successfully

--- Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
+++ Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class StudentController {
 
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
-}
+
 }
