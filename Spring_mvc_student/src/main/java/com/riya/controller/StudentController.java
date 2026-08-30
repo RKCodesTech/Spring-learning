30fbc2dcc135ca94671b9edaa9f4a54cfedfed43
RKCodesTech <riyakosta2609@gmail.com>
2026-08-30 22:05:11 +0530

    add model to Student Controller

--- Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
+++ Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
@@ -1,13 +1,17 @@
 package com.riya.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class StudentController {
 
     @RequestMapping("/hello")
-    public String hello() {
+    public String hello(Model model) {
+    	model.addAttribute("name" ,"Riya");
+    	
         return "hello";
     }
 
