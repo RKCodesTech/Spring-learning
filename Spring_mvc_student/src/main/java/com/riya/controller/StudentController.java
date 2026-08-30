2bd9d629434da65739bec822264c3b5895147346
RKCodesTech <riyakosta2609@gmail.com>
2026-08-30 23:43:51 +0530

    update StudentController get and post

--- Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
+++ Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.riya.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
@@ -11,9 +13,19 @@ public class StudentController {
 
     @GetMapping("/hello")
     public String hello(Model model) {
-    	model.addAttribute("name" ,"Riya");
-    	
+    	model.addAttribute("name" ,"Riya");	
         return "hello";
     }
-
+    @GetMapping("/student-form")
+    public String showForm() {
+    	return "student-form";
+    }
+@PostMapping("/student")
+public String addStudent(
+		@RequestParam("name") String name,
+		@RequestParam("rollno") int rollno,
+		@RequestParam("marks") int marks
+		) {
+	return "student-form";
+}
 }
