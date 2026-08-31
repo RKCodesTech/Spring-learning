ad54b704ab1c39d59edca57f21ee6039f032ee7e
RKCodesTech <riyakosta2609@gmail.com>
2026-08-31 23:50:58 +0530

    implementation

--- Spring_mvc_student/src/main/java/com/riya/model/Student.java
+++ Spring_mvc_student/src/main/java/com/riya/model/Student.java
@@ -1,5 +1,32 @@
 package com.riya.model;
 
 public class Student {
+private int rollno;
+private String  name;
+private int marks;
+public int getRollno() {
+	return rollno;
+}
+public void setRollno(int rollno) {
+	this.rollno = rollno;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getMarks() {
+	return marks;
+}
+public void setMarks(int marks) {
+	this.marks = marks;
+}
+public Student(int rollno, String name, int marks) {
+
+	this.rollno = rollno;
+	this.name = name;
+	this.marks = marks;
+}
 
 }
