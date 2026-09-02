c6daa9bdd8668bd1298e42be50ea8624f90c69b6
RKCodesTech <riyakosta2609@gmail.com>
2026-09-02 23:46:19 +0530

    debug

--- Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
+++ Spring_mvc_student/src/main/java/com/riya/controller/StudentController.java
@@ -1,33 +1,37 @@
+```java
 package com.riya.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-
 public class StudentController {
 
     @GetMapping("/hello")
     public String hello(Model model) {
-    	model.addAttribute("name" ,"Riya");	
+        model.addAttribute("name", "Riya");
         return "hello";
     }
+
     @GetMapping("/student-form")
     public String showForm() {
-    	return "student-form";
+        return "student-form";
     }
-@PostMapping("/student")
-public String addStudent(
-		@RequestParam("name") String name,
-		@RequestParam("rollno") int rollno,
-		@RequestParam("marks") int marks,
-		Model model) {
-model.addAttribute("name", name);
-model.addAttribute("rollno",rollno);
-model.addAttribute("marks" , marks);
-	return "student-form";
+
+    @PostMapping("/student")
+    public String addStudent(
+            @RequestParam("name") String name,
+            @RequestParam("rollno") int rollno,
+            @RequestParam("marks") int marks,
+            Model model) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("rollno", rollno);
+        model.addAttribute("marks", marks);
+
+        return "student-form";
+    }
 }
